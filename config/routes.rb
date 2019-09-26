40de10509ff0475756b8046b9c2d844bd876ad65
Akshay Kumar <dhupamakshay.kumar@gmail.com>
2019-09-26 23:10:55 +0530

    Adding registrations model, view, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :registrations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, path: 'auth',
              path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret',
