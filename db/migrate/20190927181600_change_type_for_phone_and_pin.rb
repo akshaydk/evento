9bc40b6ba6e44171ea064acbec70e19bf1ded03c
S, Arvind Srinivas <arvindsrinivas96@gmail.com>
2019-09-28 19:28:05 +0530

    Migration reversal added

--- db/migrate/20190927181600_change_type_for_phone_and_pin.rb
+++ db/migrate/20190927181600_change_type_for_phone_and_pin.rb
@@ -1,9 +1,17 @@
 class ChangeTypeForPhoneAndPin < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_column :users, :phone, :string
     change_column :users, :pin_code, :string
 
     add_column :users, :occupation, :string
     add_column :users, :district, :string
+  end
+
+  def down
+    remove_column :users, :district
+    remove_column :users, :occupation
+
+    change_column :users, :pin_code, :integer
+    change_column :users, :phone, :integer
   end
 end
--- db/migrate/20190928084815_change_events_columns.rb
+++ db/migrate/20190928084815_change_events_columns.rb
@@ -1,8 +1,15 @@
 class ChangeEventsColumns < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_column :event_registrations, :competition_id, :string
     change_column :event_registrations, :user_details_id, :string
 
     rename_column :event_registrations, :user_details_id, :user_id
+  end
+
+  def down
+    rename_column :event_registrations, :user_id, :user_details_id
+
+    change_column :event_registrations, :user_details_id, :integer 
+    change_column :event_registrations, :competition_id, :integer
   end
 end
